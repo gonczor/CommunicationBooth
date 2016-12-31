e06c4838c47ecdf61ee879acbc89ceb79c00fa9f
gonczor <wiktor.gonczaronek@gmail.com>
2016-12-31 15:04:19 +0100

    Added logging

--- factory/runner.sh
+++ factory/runner.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# echo "Starting factory..."
-# ./factory.exe& 
+echo "Starting factory..."
+./factory.exe > factory.log&
 
 echo "" > clients.txt
 for i in {1..5}
 do
-	./client.exe&
+	./client.exe > client$!.log&
 	PID=$!
 	echo $PID >> clients.txt
-   	echo "Started process with ID: " $PID
 done
 
 echo "Starting dispatcher..."
-./dispatcher.exe&
+./dispatcher.exe > dispatcher.log&
 
 
